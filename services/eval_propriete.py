828fb3c1bf09ccbbead2e6579e6715ce0d2e2295
aiMBF <aissasaykou@gmail.com>
2023-10-27 19:51:44 +0200

    Adding listener and check ligations for proprieties

--- services/eval_propriete.py
+++ services/eval_propriete.py
@@ -7,7 +7,23 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
-def estimer_valeur_propriete(taille_logement, ville):
+def verif_litiges(adresse_logement):
+
+    adresses_avec_litiges = [
+        "123 avenue Charles, Pierrefitte-sur-Seine",
+        "456 Rue Louis, Versailles",
+        "789 avenue Baudelaire, Mantes-la-Jolie"
+    ]
+    if adresse_logement in adresses_avec_litiges:
+        return True
+    else:
+        return False
+
+def estimer_valeur_propriete(taille_logement, ville, adresse):
+    estimation = {
+        "valeur": "",
+        "litiges": "",
+    }
     # Dictionnaire de valeurs de référence pour différentes villes
     valeurs_reference = {
         "Versailles": 300,
@@ -18,15 +34,21 @@ def estimer_valeur_propriete(taille_logement, ville):
     # Calculer la valeur estimée en fonction de la taille du logement et de la ville
     # Valeur par défaut si la ville n'est pas répertoriée
     valeur_par_metre_carre = valeurs_reference.get(ville, 200)
-    valeur_estimee = taille_logement * valeur_par_metre_carre
 
-    return valeur_estimee
+    #Estimation de la valeur de la propriete en se basant sur les prix du marche
+    estimation["valeur"] = taille_logement * valeur_par_metre_carre
+    estimation["litiges"] = verif_litiges(adresse)
+
+
+    #Voir si le logement a des litiges ou pas
+    return estimation
+
 
 
 class evaluationProprieteService(ServiceBase):
-    @rpc(Unicode, Integer, _returns=Iterable(Unicode))
-    def evaluer_propriete(ctx, ville, taille_logement):
-        valeur = estimer_valeur_propriete(taille_logement, ville)
+    @rpc(Unicode, Integer, Unicode, _returns=Iterable(Unicode))
+    def evaluer_propriete(ctx, ville, taille_logement, adresse):
+        valeur = estimer_valeur_propriete(taille_logement, ville, adresse)
         yield f"{valeur}"
 
 
