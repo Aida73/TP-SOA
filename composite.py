e82da282ca41381288efcc25c905b8c48f93f06b
Aida Sow <sowaida@ept.sn>
2023-10-28 15:26:36 +0200

    add loan decision

--- composite.py
+++ composite.py
@@ -24,6 +24,12 @@ def aproval(url, demande):
     return response.text
 
 
+def decision(valeur, propertyPrice, litiges, score, financial_cap):
+    if (valeur <= propertyPrice) and (litiges == True) and (score > 50 and (financial_cap > 0)):
+        return f"Bonjour Monsieur {client_infos['name']}, après étude de votre dossier, nous avons le plaisir de vous annoncer que le prêt pourra vous être accordé. Passer très vite à l'agence pour signer tous les documents nécessaires."
+    return f"Bonjour Monsieur {client_infos['name']}, après étude de votre dossier, nous avons le regret de vous annoncer que votre situation actuelle ne vous permet pas de contracter ce prêt. Nous vous conseillons de revenir d'ici 6mois pour une nouvelle étude."
+
+
 if __name__ == '__main__':
     print("-----------------------------------listen to new file-------------------------------------")
     event_handler = FileHandler()
@@ -36,7 +42,8 @@ if __name__ == '__main__':
                 break
             newly_created_file = event_handler.get_newly_created_file()
             if newly_created_file:
-                print("-----------------------------------load texte--------------------------------------")
+                print(
+                    "-----------------------------------load texte--------------------------------------")
                 letter_text = extract_text(newly_created_file)
                 service_extract_inf_url = "http://0.0.0.0:8002/extractInformationsService"
                 service_solvabilte_url = "http://0.0.0.0:8003/solvabiliteService"
@@ -51,12 +58,13 @@ if __name__ == '__main__':
                                 </spy:extraire_information>
                             </soapenv:Body>
                     </soapenv:Envelope>'''
-                print("-----------------------------------extraction begins--------------------------------------")
+                print(
+                    "-----------------------------------extraction begins--------------------------------------")
                 extract_data_result = aproval(
                     service_extract_inf_url, extract_enveloppeSOAP)
-                print(extract_data_result)
                 if extract_data_result:
-                    print("-----------------------------------extraction done--------------------------------------")
+                    print(
+                        "-----------------------------------extraction done--------------------------------------")
                     client_infos = json.loads(getResults(extract_data_result))
                     # check solvabilite
                     if client_infos['customerId']:
@@ -71,101 +79,55 @@ if __name__ == '__main__':
                                     </soapenv:Body>
                             </soapenv:Envelope>'''
 
-
-                        print("-----------------------------------check solvabilite begins--------------------------------------")
+                        print(
+                            "-----------------------------------check solvabilite begins--------------------------------------")
                         solvabilite_data_result = aproval(
                             service_solvabilte_url, solvabilite_enveloppeSOAP)
                         solvabilite_result = ast.literal_eval(
                             getResults(solvabilite_data_result))
-                        # solvable = solvabilite_result['solvable']
-                        print(solvabilite_result)
                     # check propriete
                     if client_infos['description']:
-                        print(client_infos['description']['address']['completeAddress'])
-                        evalPropriete_enveloppeSOAP = f'''\
-                            <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:spy="spyne.examples.hello">
-                                <soapenv:Header/>
-                                    <soapenv:Body>
-                                        <spy:evaluer_propriete>
-                                            <!--Optional:-->
-                                            <spy:ville>{client_infos['description']['address']['town']}</spy:ville>
-                                            <!--Optional:-->
-                                            <spy:taille_logement>{int(client_infos['description']['surfaceArea'].split('m')[0])}</spy:taille_logement>
-                                            <!--Optional:-->
-                                            <spy:adresse>{client_infos['description']['address']['completeAddress']}</spy:adresse>
-                                        </spy:evaluer_propriete>
-                                    </soapenv:Body>
-                            </soapenv:Envelope>'''
+                        if 'completeAdress' in client_infos['description']['address'].keys():
+                            evalPropriete_enveloppeSOAP = f'''\
+                                <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:spy="spyne.examples.hello">
+                                    <soapenv:Header/>
+                                        <soapenv:Body>
+                                            <spy:evaluer_propriete>
+                                                <!--Optional:-->
+                                                <spy:ville>{client_infos['description']['address']['town']}</spy:ville>
+                                                <!--Optional:-->
+                                                <spy:taille_logement>{int(client_infos['description']['surfaceArea'].split('m')[0])}</spy:taille_logement>
+                                                <!--Optional:-->
+                                                <spy:adresse>{client_infos['description']['address']['completeAdress']}</spy:adresse>
+                                            </spy:evaluer_propriete>
+                                        </soapenv:Body>
+                                </soapenv:Envelope>'''
+                        else:
+                            evalPropriete_enveloppeSOAP = f'''\
+                                <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:spy="spyne.examples.hello">
+                                    <soapenv:Header/>
+                                        <soapenv:Body>
+                                            <spy:evaluer_propriete>
+                                                <!--Optional:-->
+                                                <spy:ville>{client_infos['description']['address']['town']}</spy:ville>
+                                                <!--Optional:-->
+                                                <spy:taille_logement>{int(client_infos['description']['surfaceArea'].split('m')[0])}</spy:taille_logement>
+                                                <!--Optional:-->
+                                                <spy:adresse>{client_infos['description']['address']['completeAddress']}</spy:adresse>
+                                            </spy:evaluer_propriete>
+                                        </soapenv:Body>
+                                </soapenv:Envelope>'''
                         print(
                             "-----------------------------------evaluation propriete begins--------------------------------------")
                         evalPropriete_data_result = aproval(
                             service_evalPropriete_url, evalPropriete_enveloppeSOAP)
-                        # evalPropriete_result = getResults(evalPropriete_data_result)
-                        print(evalPropriete_data_result)
+                        evalPropriete_result = ast.literal_eval(getResults(
+                            evalPropriete_data_result))
+                        # print(evalPropriete_result)
+                    # ----------------------------------------Start approal-------------------------------------------------------#
+                    print(decision(evalPropriete_result['valeur'], client_infos['propertyPrice'],
+                          evalPropriete_result['litiges'], solvabilite_result['score'], solvabilite_result['financial_cap']))
+
                 event_handler.set_stop_condition(True)
     except KeyboardInterrupt:
         pass
-    """service_extract_inf_url = "http://0.0.0.0:8002/extractInformationsService"
-    service_solvabilte_url = "http://0.0.0.0:8003/solvabiliteService"
-    service_evalPropriete_url = "http://0.0.0.0:8004/evaluationProprieteService"
-    extract_enveloppeSOAP = f'''\
-    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:spy="spyne.examples.hello">
-        <soapenv:Header/>
-            <soapenv:Body>
-                <spy:extraire_information>
-                    <!--Optional:-->
-                    <spy:demande>{letter_text}</spy:demande>
-                </spy:extraire_information>
-            </soapenv:Body>
-    </soapenv:Envelope>'''
-    print("-----------------------------------extraction begins--------------------------------------")
-    extract_data_result = aproval(
-        service_extract_inf_url, extract_enveloppeSOAP)
-    print(extract_data_result)
-    if extract_data_result:
-        print("-----------------------------------extraction done--------------------------------------")
-        client_infos = json.loads(getResults(extract_data_result))
-        # check solvabilite
-        if client_infos['customerId']:
-            solvabilite_enveloppeSOAP = f'''\
-            <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:spy="spyne.examples.hello">
-                <soapenv:Header/>
-                    <soapenv:Body>
-                        <spy:etudier_solvabilite>
-                            <!--Optional:-->
-                            <spy:clientId>{client_infos['customerId']}</spy:clientId>
-                        </spy:etudier_solvabilite>
-                    </soapenv:Body>
-            </soapenv:Envelope>'''
-            print("-----------------------------------check solvabilite begins--------------------------------------")
-            solvabilite_data_result = aproval(
-                service_solvabilte_url, solvabilite_enveloppeSOAP)
-            solvabilite_result = ast.literal_eval(
-                getResults(solvabilite_data_result))
-            # solvable = solvabilite_result['solvable']
-            print(solvabilite_result)
-        # check propriete
-        if client_infos['description']:
-            print(client_infos['description']['address']['completeAddress'])
-            evalPropriete_enveloppeSOAP = f'''\
-            <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:spy="spyne.examples.hello">
-                <soapenv:Header/>
-                    <soapenv:Body>
-                        <spy:evaluer_propriete>
-                            <!--Optional:-->
-                            <spy:ville>{client_infos['description']['address']['town']}</spy:ville>
-                            <!--Optional:-->
-                            <spy:taille_logement>{int(client_infos['description']['surfaceArea'].split('m')[0])}</spy:taille_logement>
-                            <!--Optional:-->
-                            <spy:adresse>{client_infos['description']['address']['completeAddress']}</spy:adresse>
-                        </spy:evaluer_propriete>
-                    </soapenv:Body>
-            </soapenv:Envelope>'''
-            print("-----------------------------------evaluation propriete begins--------------------------------------")
-            evalPropriete_data_result = aproval(
-                service_evalPropriete_url, evalPropriete_enveloppeSOAP)
-            # evalPropriete_result = getResults(evalPropriete_data_result)
-            print(evalPropriete_data_result)
-
-    else:
-        print("Some problem occurs. You can check your Api Key.") """
--- file_listener.py
+++ file_listener.py
@@ -5,8 +5,7 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import re
 
-# Define the directory to monitor (assuming it's in the same directory as your script)
-folder_to_watch = "./depots"  # Replace this with the directory you want to monitor
+folder_to_watch = "./depots"
 
 # Get the absolute path to the folder
 folder_path = os.path.abspath(folder_to_watch)
@@ -20,9 +19,6 @@ def clean_text(text):
 
 def extract_text(file_path):
     try:
-        #file_path = FileHandler().on_created
-        #print(file_path)
-        #print(type(file_path))
         text = textract.process(file_path).decode("utf-8")
         return clean_text(text)
     except Exception as e:
@@ -37,7 +33,6 @@ class FileHandler(FileSystemEventHandler):
 
     def on_created(self, event):
         if not event.is_directory:
-            #print(f"Newly created file: {event.src_path}")
             self.newly_created_file = event.src_path
 
     def get_newly_created_file(self):
@@ -48,17 +43,3 @@ class FileHandler(FileSystemEventHandler):
 
     def set_stop_condition(self, value):
         self.should_stop = value
-
-"""if __name__ == "__main__":
-    event_handler = FileHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path=folder_path)
-    observer.start()
-    try:
-        while True:
-            if event_handler.newly_created_file:
-                print("Newly created file:", event_handler.newly_created_file)
-
-    except KeyboardInterrupt:
-        observer.stop()
-        observer.join() """
